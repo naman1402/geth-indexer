53d610c83f1596b050596320301b79edf98152d8
naman mohnani <mohnaninaman@gmail.com>
2025-01-03 11:47:57 +0530

    building the indexing logic

--- indexer/indexer.go
+++ indexer/indexer.go
@@ -2,8 +2,52 @@ package indexer
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/naman1402/geth-indexer/subsrciber"
 )
 
-func Index(eventCh chan *subsrciber.Event, db *sql.DB, quit chan bool) {}
+func Index(eventCh chan *subsrciber.Event, db *sql.DB, quit chan bool) {
+
+	for {
+		select {
+		case e := <-eventCh:
+			query := generateQuery(strings.ToLower(e.Name), e)
+			go executeQuery(db, query)
+		case q := <-quit:
+			if q {
+				return
+			}
+		}
+	}
+}
+
+func generateQuery(table string, param *subsrciber.Event) string {
+
+	columns := len(param.Data)
+	fieldSlice := make([]string, 0, columns)
+	fields := "name, blockNumber, blockHash, contract, "
+
+	for fields := range param.Data {
+		fieldSlice = append(fieldSlice, fields)
+	}
+
+	fields += strings.Join(fieldSlice, " ,")
+
+	//
+	values := addValues(fieldSlice, param)
+	return fmt.Sprintf(`INSERT INTO %s (%s) VALUES (%s)`, table, fields, values)
+
+}
+
+func addValues(keys []string, params *subsrciber.Event) string {
+	values := make([]string, 0, len(keys)+4)
+	values = append(values, fmt.Sprintf("%v", params.Name), fmt.Sprintf("%v", params.BlockNumber), fmt.Sprintf("%v", params.BlockHash), fmt.Sprintf("%v", params.Contract))
+
+	for _, k := range keys {
+		values = append(values, fmt.Sprintf("%v", params.Data[k]))
+	}
+
+	return strings.Join(values, " ,")
+}
